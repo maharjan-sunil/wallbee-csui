96f3441cbc0ff209a4ebed07473145bbc024b69d
sunil <sunil@codebee.dk>
2021-02-21 15:41:23 +0545

    Updates

---
+++ precache-manifest.16582529ef9f2031c07fcc37031e836d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8449ec8f1c9eeca46c7214c35b0204c",
+    "url": "/wallbee-csui/index.html"
+  },
+  {
+    "revision": "c5a7d11612a2827d6c35",
+    "url": "/wallbee-csui/static/css/main.89b46129.chunk.css"
+  },
+  {
+    "revision": "7dbb4dcd91df8d2df037",
+    "url": "/wallbee-csui/static/js/2.ad6f5386.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/wallbee-csui/static/js/2.ad6f5386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a7d11612a2827d6c35",
+    "url": "/wallbee-csui/static/js/main.ec1b764f.chunk.js"
+  },
+  {
+    "revision": "63bfd810acfe14cf6d6e",
+    "url": "/wallbee-csui/static/js/runtime-main.77199d01.js"
+  }
+]);
