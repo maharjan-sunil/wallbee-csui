e7f5e2509844d438aa0508e1617e40083e798d6b
Sunil Maharjan <sunil.maharjan1998@gmail.com>
2022-03-27 21:16:21 -0400

    master update from Canada

--- src/components/PhotoWall.js
+++ src/components/PhotoWall.js
@@ -4,19 +4,25 @@ import { Link } from "react-router-dom";
 import Photo from "./Photo";
 import PropTypes from "prop-types";
 
-function PhotoWall(props) {
-  return (
-    <div>
-      <Link className="addIcon" to="/addPhotos" />
-      <div className="photoGrid">
-        {props.posts.sort((x, y) => {
-          return y.id - x.id;
-        }).map((post, index) => (
-          <Photo key={index} post={post} index={index} {...props} />
-        ))}
+class PhotoWall extends React.Component {
+  constructor() {
+  }
+  render() {
+    return (
+      <div>
+        <Link className="addIcon" to="/addPhotos" />
+        <div className="photoGrid">
+          {this.props.posts
+            .sort((x, y) => {
+              return y.id - x.id;
+            })
+            .map((post, index) => (
+              <Photo key={index} post={post} index={index} {...props} />
+            ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 PhotoWall.propTypes = {
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Title extends Component {
+  componentDidMount() {
+    console.log("this is componentDidMount");
+  }
+
   render() {
     return <h1> {this.props.Title} </h1>;
   }
